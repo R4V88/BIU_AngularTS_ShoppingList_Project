95b93054a18fa91dbd9f59d3bbe5116a5d5a2c80
Rafal <sochacki.rfl@gmail.com>
2021-06-24 21:04:15 +0200

    project structure refactored

--- shopping-list/src/app/app.module.ts
+++ shopping-list/src/app/app.module.ts
@@ -2,9 +2,9 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {ProductsPoolComponent} from './products-pool/products-pool.component';
-import {ProductComponent} from './product/product.component';
-import {ProductsListComponent} from './products-list/products-list.component';
+import {ProductsPoolComponent} from './products/products-pool/products-pool.component';
+import {ProductComponent} from './products/product/product.component';
+import {ProductsListComponent} from './products/products-list/products-list.component';
 
 @NgModule({
   declarations: [
--- shopping-list/src/app/products/products-pool/products-pool.component.ts
+++ shopping-list/src/app/products/products-pool/products-pool.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Product} from "../product";
+import {Product} from "../product/product";
 
 @Component({
   selector: 'app-products-pool',
